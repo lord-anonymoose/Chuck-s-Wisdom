400b8d4333964622273f82f5f9ffe522e7a8f719
Philipp Lazarev <philipp.lazareff@gmail.com>
2024-07-08 22:58:07 +0300

    Added checking for Jailbreak

--- Chuck's Wisdom/Delegates/SceneDelegate.swift
+++ Chuck's Wisdom/Delegates/SceneDelegate.swift
@@ -17,6 +17,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                                
         let window = UIWindow(windowScene: scene)
         
+        guard !isJailbroken() else {
+            let navigationController = JailbreakViewController()
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+            self.window = window
+            return
+        }
+
         let tabBarController = UITabBarController()
         var controllers = [UIViewController]()
         
@@ -42,7 +50,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let navigationController = UINavigationController()
         navigationController.pushViewController(tabBarController, animated: true)
-        
+         
         window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
--- Chuck's Wisdom/Services/Extensions.swift
+++ Chuck's Wisdom/Services/Extensions.swift
@@ -17,3 +17,19 @@ extension UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
 }
+
+func isJailbroken() -> Bool {
+    if let urlScheme = NSURL(string: "cydia://"),
+       UIApplication.shared.canOpenURL(urlScheme as URL) {
+        return true
+    }
+    
+    do {
+        try "text".data(using: .utf8)?.write(to: URL(filePath: "/test.txt"))
+        return true
+    } catch {
+        
+    }
+    
+    return false
+}
